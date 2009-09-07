23173ad8e997fc5444cf97085adfff2754aaa286
lodle.05 <lodle.05@dd826556-9b40-11de-bff5-93fd3790126f>
2009-09-07 05:48:33 +0000

    first package src

---
+++ src/game/server/py/ge_pytest.cpp
@@ -0,0 +1,43 @@
+///////////// Copyright © 2009 LodleNet. All rights reserved. /////////////
+//
+//   Project     : Server
+//   File        : ge_pygameplay.cpp
+//   Description :
+//      [TODO: Write the purpose of ge_pygameplay.cpp.]
+//
+//   Created On: 8/24/2009 3:39:13 PM
+//   Created By: Mark Chandler <mailto:mark@moddb.com>
+////////////////////////////////////////////////////////////////////////////
+
+#include "cbase.h"
+#include "ge_pytest.h"
+
+CMYPyHandle *g_PyHandle = NULL;
+CMYPyHandle *GetPyHandle()
+{
+	return g_PyHandle;
+}
+
+
+CMYPyHandle::CMYPyHandle()
+{
+	g_MyHandle = this;
+	//make sure we call this!
+	Register();
+}
+
+void CMYPyHandle::Init()
+{
+	try
+	{
+		ExecFile("test.py");
+	}
+	catch (...)
+	{
+		HandlePythonException();
+	}
+}
+
+void CMYPyHandle::Shutdown()
+{
+}
